aec2a1bad22946c9a6d2185911767d8028d136a6
Nicholas Utikal <nicholasutikal@gmail.com>
2018-02-19 14:49:31 +0100

    schema modified after booking migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219131800) do
+ActiveRecord::Schema.define(version: 20180219133030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bathrooms", force: :cascade do |t|
+    t.string "address"
+    t.text "description"
+    t.integer "price"
+    t.boolean "handicapped"
+    t.string "style"
+    t.integer "toilet_paper"
+    t.boolean "wipes"
+    t.boolean "baby"
+    t.boolean "bidet"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bathrooms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +48,5 @@ ActiveRecord::Schema.define(version: 20180219131800) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bathrooms", "users"
 end
