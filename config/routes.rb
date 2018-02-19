f7787714f2e83042644cc18d6595d5816f215894
Vini Tiet <tiet@mira-am.com>
2018-02-19 15:51:11 +0100

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/edit'
+
   get 'bookings/new'
 
   get 'bookings/create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219133030) do
+ActiveRecord::Schema.define(version: 20180219140547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,18 @@ ActiveRecord::Schema.define(version: 20180219133030) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "plz"
+    t.string "city"
     t.index ["user_id"], name: "index_bathrooms_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "bathroom_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bathroom_id"], name: "index_bookings_on_bathroom_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,9 +59,13 @@ ActiveRecord::Schema.define(version: 20180219133030) do
     t.string "last_name"
     t.string "gender"
     t.string "age"
+    t.string "plz"
+    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bathrooms", "users"
+  add_foreign_key "bookings", "bathrooms"
+  add_foreign_key "bookings", "users"
 end
