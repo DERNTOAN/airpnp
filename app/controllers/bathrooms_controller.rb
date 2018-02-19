b48dc96ce85fa6293548c30bc66ca48e7ce8bb2d
Nicholas Utikal <nicholasutikal@gmail.com>
2018-02-19 15:42:27 +0100

    added CRUD to bathroom controller

--- app/controllers/bathrooms_controller.rb
+++ app/controllers/bathrooms_controller.rb
@@ -1,14 +1,24 @@
 class BathroomsController < ApplicationController
   def index
+    @bathroom = Bathroom.all
   end
 
   def show
+    @bathroom = Bathroom.find(params[:id])
+    @user = User.new
   end
 
   def new
+    @bathroom = Bathroom.new
   end
 
   def create
+    @bathroom = Bathroom.new(bathroom_params)
+    if @bathroom.save
+      redirect_to bathroom_path(@bathroom)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +29,11 @@ class BathroomsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def bathroom_params
+    params.require(:bathroom).permit(:address ,:description, :price,:photo, :photo_cache, :handicapped, :style, :toilet_paper, :wipes, :baby, :bidet, :user_id)
+  end
 end
+
+
