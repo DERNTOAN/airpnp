c14599fb727ed9d62c7dbb3533a48d2c9355aab7
Henrik Dittmar <henrik.dittmar@gmx.de>
2018-02-19 16:11:24 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20180219142958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "plz"
+    t.string "city"
     t.index ["user_id"], name: "index_bathrooms_on_user_id"
   end
 
