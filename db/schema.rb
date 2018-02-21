2c79043ff50c4755ed71601c41886e5f4a9c8e5a
Anton Castell <castellanton@gmail.com>
2018-02-21 11:53:08 +0100

    added lat and lng to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221092110) do
+ActiveRecord::Schema.define(version: 20180221103221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 20180221092110) do
     t.string "gender"
     t.string "age"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
