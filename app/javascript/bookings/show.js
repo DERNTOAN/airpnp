e607d490b74a118e6b94631bef1c2420db4712ec
Henrik Dittmar <henrik.dittmar@gmx.de>
2018-02-21 16:33:06 +0100

    timer working no style

--- app/javascript/bookings/show.js
+++ app/javascript/bookings/show.js
@@ -1,7 +1,32 @@
+function getTimeRemaining(endtime){
+  let t = endtime - Date.parse(new Date());
+  let seconds = Math.floor( (t/1000) % 60 );
+  let minutes = Math.floor( (t/1000/60) % 60 );
+  let hours = Math.floor( (t/(1000*60*60)) % 24 );
+  let days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
 function countdownTimer(){
+  const timer = document.getElementById("booking-timer");
+  let created = timer.dataset["created"];
+  let endtime = new Date(parseFloat(created));
+  console.log(endtime);
+
+
   setInterval(function(){
-    document.getElementById("booking-timer").innerHTML = (new Date()).toLocaleTimeString();
-}, 1000);
+    let t = getTimeRemaining(endtime)
+    timer.innerHTML = '00: ' + t.minutes + ':' + t.seconds;
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  }, 1000);
 }
 
 
