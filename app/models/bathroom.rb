c071738227ad3351c7d59f27703df3ef2df7f8c0
Nicholas Utikal <nicholasutikal@gmail.com>
2018-02-22 13:54:12 +0100

    review controller + model

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -2,6 +2,7 @@ class Bathroom < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   belongs_to :user
   has_many :bookings
+  has_many :reviews
 
   validates :address, presence: true
   validates :title, presence: true
@@ -11,14 +12,14 @@ class Bathroom < ApplicationRecord
   validates :price, presence: true
   validates :user, presence: :true
   validates :description, presence: true
-  
+
   geocoded_by :complete_address
   after_validation :geocode
 
   def complete_address
     address + " " + plz + " " + city
   end
-  
+
   def self.class_string(b)
     string = "data-tp=#{b.toilet_paper} data-price=#{b.price} data-baby=#{b.baby} data-bidet=#{b.bidet} data-handicapped=#{b.handicapped} data-style=#{b.style}"
     return string
